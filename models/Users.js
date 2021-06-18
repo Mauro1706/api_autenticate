310f5de4f6054c2e7f10e8614042017a4a3872a2
Mauro Wendel Rodrigues Costa <costa.mauro@eb.mil.br>
2021-06-18 11:48:35 -0300

    alterações na api

--- models/Users.js
+++ models/Users.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
 const Users = new mongoose.Schema({
-        name: { type: String, require: true },
-        password: { type: String, require: true },
-        cpf: { type: String, minlength: 11, require: false },
-        data_registro: { type: Date, default: Date.now },
-    },
-    {
-        timestamps: true,
-    });
+    name: { type: String, require: true },
+    password: { type: String, require: true },
+    cpf: { type: String, require: false },
+    isFaceta: { type: Boolean, required: true, default: 0 },
+    data: { type: Date, default: Date.now },
+},
+{
+    timestamps: true,
+});
 
 mongoose.model('users', Users);
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 const authMiddleware = require('./auth');
 
 const router = express.Router();
+
+require('../models/Users');
+const Users = mongoose.model('users');
+
+mongoose.connect('mongodb://localhost/autenticate', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(() => {
+  console.log("Conexão com MongoDB realizada com sucesso!");
+}).catch((erro) => {
+  console.log("Conexão com MongoDB falhou!");
+});
 
 router.post('/authenticate', (req, res) => {
   const user = {
@@ -19,29 +34,48 @@ router.post('/authenticate', (req, res) => {
   });
 });
 
+router.post("/create", (req, res) => {
+  const users = Users.create(req.body, (err) => {
+    if (err) return res.status(400).json({
+      error: true,
+      message: "Erro: Usuário não foi cadastrada no banco de dados."
+    });
+
+    return res.status(200).json({
+      error: false,
+      message: "Usuário cadastrado com sucesso!"
+    });
+  });
+});
+
 /**
  * Private route
  */
-router.use(authMiddleware);
+//router.use(authMiddleware);
 
-router.get('/users', async (req, res) => {
-  return res.json([
-    {
-      id: 1,
-      name: 'Mateus Silva',
-      website: 'https://devacademy.com.br',
-    },
-    {
-      id: 2,
-      name: 'Mark Zuckerberg',
-      website: 'https://facebook.com',
-    },
-    {
-      id: 3,
-      name: 'Bill Gates',
-      website: 'https://www.microsoft.com',
-    },
-  ]);
+router.get("/users", (req, res) => {
+  Users.find({}).then((users) => {
+    return res.json(users);
+  }).catch((err) => {
+    return res.status(400).json({
+      error: true,
+      message: "Nenhum usuário encontrado!"
+    });
+  });
+});
+
+router.delete("/delete/:id",  (req, res) => {
+  const users = Users.deleteOne({ _id: req.params.id }, (err) => {
+    if (err) return res.status(400).json({
+      error: true,
+      message: "Erro: Usuário não foi excluido do banco de dados."
+    });
+
+    return res.status(200).json({
+      error: false,
+      message: "Usuário excluido com sucesso!"
+    });
+  });
 });
 
 module.exports = router;
